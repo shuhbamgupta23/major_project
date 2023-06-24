c6a45a7d923554e0e3257515d8d8021d1db432cc
unknown <mrjanuaryx@gmail.com>
2023-06-24 14:02:13 +0530

    update

--- frontend/src/component/About.js
+++ frontend/src/component/About.js
@@ -2,13 +2,21 @@ import React from "react";
 
 const About = () => {
   return (
-    <div style={{ width: "100%", height: "45vh", display:'flex', alignItems:"center", justifyContent:"center"}}>
+    <div
+      style={{
+        width: "100%",
+        height: "45vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <div>
-        <h1>About us </h1>
-        <h3>Shubham Gupta</h3>
-        <h3>Shashank Jaiswal</h3>
-        <h3>Jasmeet Singh</h3>
-        <h3>Rohit Dhiman</h3>
+        <h1>About us</h1>
+        <h3>Shubham Gupta&nbsp;:&nbsp;03613202719</h3>
+        <h3>Shashank Jaiswal&nbsp;:&nbsp;04313202719</h3>
+        <h3>Jasmeet Singh&nbsp;:&nbsp;02313202719</h3>
+        <h3>Rohit Dhiman&nbsp;:&nbsp;04413202719</h3>
       </div>
     </div>
   );
--- frontend/src/component/Contact.js
+++ frontend/src/component/Contact.js
@@ -18,15 +18,17 @@ const Contact = () => {
         </h3>
         <h3>
           Shashank Jaiswal&nbsp;:&nbsp;
-          <a href="https://www.linkedin.com/in/shubhamgupta2000/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/shashankjaiswal07/">LinkedIn</a>
         </h3>
         <h3>
           Jasmeet Singh&nbsp;:&nbsp;
-          <a href="https://www.linkedin.com/in/shubhamgupta2000/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/jasmeets073/">LinkedIn</a>
         </h3>
         <h3>
           Rohit Dhiman&nbsp;:&nbsp;
-          <a href="https://www.linkedin.com/in/shubhamgupta2000/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/rohit-dhiman-3585301ab/">
+            LinkedIn
+          </a>
         </h3>
       </div>
     </div>
